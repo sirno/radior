d826a324ee3092433a153d3e44700955bdf1828e
Nicolas Ochsner <nicolas.ochsner@env.ethz.ch>
2021-07-23 16:22:02 +0200

    Updates.

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,16 @@ use chrono::Duration;
 use cursive::event::Event;
 use cursive::views::{TextContent, TextView};
 use cursive::{Cursive, CursiveExt};
+use std::cmp::{max, min};
 use std::env;
 use std::ffi::CStr;
 use std::fs;
+// use std::time::SystemTime;
 use toml::Value;
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+const TILE_SIZE: i32 = 20;
 
 macro_rules! cstr {
     ($s:expr) => {{
@@ -28,7 +32,7 @@ pub trait clock {
         let s = self.get_seconds();
         let ds = s % 60;
         let dm = s / 60 % 60;
-        let dh = s / 60;
+        let dh = s / 3600;
         return format!("{:02}:{:02}:{:02}", dh, dm, ds);
     }
 }
@@ -68,18 +72,36 @@ unsafe fn readcstrf(s: *const ::std::os::raw::c_char) -> f32 {
 }
 
 unsafe fn get_display(ctx: *mut mpv_handle) -> String {
-    let title: String = readcstr(mpv_get_property_string(ctx, cstr!("media-title")));
+    let mut title: String = readcstr(mpv_get_property_string(ctx, cstr!("media-title")));
     let vol: f32 = readcstrf(mpv_get_property_string(ctx, cstr!("volume")));
     let time: f32 = readcstrf(mpv_get_property_string(ctx, cstr!("time-pos")));
     let duration: f32 = readcstrf(mpv_get_property_string(ctx, cstr!("duration")));
+    let title_size = title.chars().count() as i32;
+    if title_size > TILE_SIZE {
+        let linger = 4;
+        let mut offset = ((2. * time) as i32 % (title_size - TILE_SIZE + 2 * linger)) - linger;
+        offset = max(min(offset, title_size - TILE_SIZE), 0);
+        let start = offset as usize;
+        let end = (TILE_SIZE + offset) as usize;
+        title = title[start..end].to_string();
+    }
     return format!(
-        "\n Title: {:15} \n Volume: {} \n {} / {} \n ",
+        "\n Title: {:size$} \n Volume: {} \n {} / {} \n ",
         title,
         vol,
         Duration::seconds(time as i64).clock(),
         Duration::seconds(duration as i64).clock(),
+        size = TILE_SIZE as usize,
     );
 }
+
+// fn get_dancer(t: SystemTime) -> String {
+//     let s = t.elapsed().unwrap().as_secs() as u32;
+//     if s % 2 == 0 {
+//         return " (._.)/\n<(  (  \n  /  \\ \n".to_string();
+//     }
+//     return "\\(._.) \n  )  )>\n  /  \\ \n".to_string();
+// }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -125,6 +147,19 @@ fn main() {
 
         siv.add_layer(central_text);
 
+        // let t = SystemTime::now();
+        // let dancer = TextContent::new(get_dancer(t));
+        // let dancer_view = TextView::new_with_content(dancer.clone());
+
+        // let layout = FixedLayout::new()
+        //     .child(Rect::from_size((0, 0), (1, 1)), TextView::new("/"))
+        //     .child(Rect::from_size((20, 0), (1, 1)), TextView::new(r"\"))
+        //     .child(Rect::from_size((0, 4), (1, 1)), TextView::new(r"\"))
+        //     .child(Rect::from_size((20, 4), (1, 1)), TextView::new("/"))
+        //     .child(Rect::from_size((7, 1), (7, 3)), dancer_view);
+
+        // siv.add_fullscreen_layer(layout);
+
         siv.add_global_callback('q', move |s| {
             s.quit();
             mpv_command(ctx, [cstr!("quit"), std::ptr::null()].as_mut_ptr());
@@ -157,11 +192,13 @@ fn main() {
         });
 
         let content_refresh = content.clone();
+        // let dancer_refresh = dancer.clone();
         siv.add_global_callback(Event::Refresh, move |_s| {
             content_refresh.set_content(get_display(ctx));
+            // dancer_refresh.set_content(get_dancer(t))
         });
 
-        siv.set_autorefresh(true);
+        siv.set_fps(10);
         siv.run();
 
         mpv_terminate_destroy(ctx);
